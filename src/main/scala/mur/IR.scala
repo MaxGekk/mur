769688677bec8fbd83f81daa58e253c62db5d0f9
Maxim Gekk <max.gekk@gmail.com>
2017-05-12 18:05:59 +0200

    IR for statements and program

--- src/main/scala/mur/IR.scala
+++ src/main/scala/mur/IR.scala
@@ -1,68 +1,8 @@
 package mur
 
-sealed trait Result
-case class Num(value: Int) extends Result
-case class Real(value: Double) extends Result
+sealed trait Stmt
+case class Print(str: String) extends Stmt
+case class Out(expr: Expr) extends Stmt
+case class VarDef(identifer: String, expr: Expr) extends Stmt
 
-trait Expr {
-  def calc: Result
-}
-
-case class Id(name: String) extends Expr {
-  def calc = ??? // Look up the symbol table
-}
-
-sealed trait Number extends Expr
-case class IntNumber(n: Int) extends Number {
-  def calc: Result = Num(n)
-}
-case class RealNumber(n: Double) extends Number {
-  def calc: Result = Real(n)
-}
-
-sealed trait Op extends Expr {
-  def left: Expr
-  def right: Expr
-}
-
-case class Plus(left: Expr, right: Expr) extends Op {
-  def calc: Result = (left.calc, right.calc) match {
-    case (Num(l), Num(r)) => Num(l + r)
-    case (Real(l), Num(r)) => Real(l + r)
-    case (Num(l), Real(r)) => Real(l + r)
-    case (Real(l), Real(r)) => Real(l + r)
-  }
-}
-case class Minus(left: Expr, right: Expr) extends Op {
-  def calc: Result = (left.calc, right.calc) match {
-    case (Num(l), Num(r)) => Num(l - r)
-    case (Real(l), Num(r)) => Real(l - r)
-    case (Num(l), Real(r)) => Real(l - r)
-    case (Real(l), Real(r)) => Real(l - r)
-  }
-}
-case class Mul(left: Expr, right: Expr) extends Op {
-  def calc: Result = (left.calc, right.calc) match {
-    case (Num(l), Num(r)) => Num(l * r)
-    case (Real(l), Num(r)) => Real(l * r)
-    case (Num(l), Real(r)) => Real(l * r)
-    case (Real(l), Real(r)) => Real(l * r)
-  }
-}
-case class Div(left: Expr, right: Expr) extends Op {
-  def calc: Result = (left.calc, right.calc) match {
-    case (Num(l), Num(r)) => Num(l / r)
-    case (Real(l), Num(r)) => Real(l / r)
-    case (Num(l), Real(r)) => Real(l / r)
-    case (Real(l), Real(r)) => Real(l / r)
-  }
-}
-case class Pow(left: Expr, right: Expr) extends Op {
-  import scala.math.pow
-  def calc: Result = (left.calc, right.calc) match {
-    case (Num(l), Num(r)) => Num(pow(l, r).intValue)
-    case (Real(l), Num(r)) => Real(pow(l, r))
-    case (Num(l), Real(r)) => Real(pow(l, r))
-    case (Real(l), Real(r)) => Real(pow(l, r))
-  }
-}
+case class Program(stmts: Seq[Stmt])
