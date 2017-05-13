0e4509e2ae3ffda23eda8b125e26c7f24bb57414
Maxim Gekk <max.gekk@gmail.com>
2017-05-13 17:00:57 +0200

    Parser combinator for mul, div and pow

--- src/main/scala/mur/Parsers.scala
+++ src/main/scala/mur/Parsers.scala
@@ -5,18 +5,39 @@ import scala.util.parsing.input.CharSequenceReader
 
 trait Parsers extends RegexParsers {
 
-  def expr:Parser[Expr] = """\d+""".r ^^ {
+  def integer: Parser[Literal] = """-?\d+""".r ^^ {
     s => Literal(s.toInt)
   }
+  def brackets = "(" ~> expr <~ ")"
+
+  def operand = (integer | brackets)
+
+  def plus:Parser[Plus] = operand ~ "+" ~ operand ^^ {
+    case (x ~ "+" ~ y) => Plus(x, y)
+  }
+
+  def mul:Parser[Mul] = operand ~ "*" ~ operand ^^ {
+    case (x ~ "*" ~ y) => Mul(x, y)
+  }
+
+  def div:Parser[Div] = operand ~ "/" ~ operand ^^ {
+    case (x ~ "/" ~ y) => Div(x, y)
+  }
+
+  def pow:Parser[Pow] = operand ~ "^" ~ operand ^^ {
+    case (x ~ "^" ~ y) => Pow(x, y)
+  }
+
+  def expr:Parser[Expr] = (plus | mul | div | pow | integer | brackets)
 }
 
 object Parsers extends Parsers {
 
-  def parseExpression(s: CharSequence): Expr = {
-    parseExpression(new CharSequenceReader(s))
+  def parseExpr(s: CharSequence): Expr = {
+    parseExpr(new CharSequenceReader(s))
   }
 
-  def parseExpression(input: CharSequenceReader): Expr = {
+  def parseExpr(input: CharSequenceReader): Expr = {
     parsePhrase(input) match {
       case Success(t, _) => t
       case NoSuccess(msg, next) => throw new IllegalArgumentException(
--- src/test/scala/mur/ParserTests.scala
+++ src/test/scala/mur/ParserTests.scala
@@ -5,7 +5,35 @@ import org.scalatest.{FreeSpec, Matchers}
 class ParserTests extends FreeSpec with Matchers {
   "Parsing of " - {
     " integer" in {
-      assert(Parsers.parseExpression("42") == Literal(42))
+      assert(Parsers.parseExpr("42") == Literal(42))
+    }
+    "a simple sum" in {
+      assert(Parsers.parseExpr("2 + 3") == Plus(Literal(2), Literal(3)))
+    }
+    "a simple multiplication" in {
+      assert(Parsers.parseExpr("2 * 3") == Mul(Literal(2), Literal(3)))
+    }
+    "a simple div" in {
+      assert(Parsers.parseExpr("-1 / 4") == Div(Literal(-1), Literal(4)))
+    }
+    "a simple pow" in {
+      assert(Parsers.parseExpr("(-1) ^ 10") == Pow(Literal(-1), Literal(10)))
+    }
+  }
+  "Parsing a complex expression of " - {
+    " integers" in {
+
+      assert(Parsers.parseExpr("(2 * 3) * (4 + 5)") ==
+        Mul(
+          Mul(
+            Literal(2),
+            Literal(3)),
+          Plus(
+            Literal(4),
+            Literal(5)
+          )
+        )
+      )
     }
   }
 }
