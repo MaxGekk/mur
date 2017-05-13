483c58609a28d6012a5ebaf9e82df3fff7e7c4af
Maxim Gekk <max.gekk@gmail.com>
2017-05-13 12:31:01 +0200

    Added an example

--- src/main/scala/mur/Main.scala
+++ src/main/scala/mur/Main.scala
@@ -1,7 +1,40 @@
 package mur
 
 object Main {
+  val text =
+    """
+      |var n = 500
+      |var sequence = map({0, n}, i -> (-1)^i / (2 * i + 1))
+      |var pi = 4 * reduce(sequence, 0, x y -> x + y)
+      |print "pi = "
+      |out pi
+    """.stripMargin
+  val prog = Program(Seq(
+    VarDef("n", Literal(500)),
+    VarDef("sequence",
+      MapSeq(
+        Sequence(Literal(0), Id("n")),
+        Id("i"),
+        Div(
+          Pow(Literal(-1), Id("i")),
+          Plus(Mul(Literal(2), Id("i")), Literal(1))
+        )
+      )
+    ),
+    VarDef("pi",
+      Mul(Literal(4),
+        ReduceSeq(
+          Id("sequence"), Literal(0), Id("x"), Id("y"),
+          Plus(Id("x"), Id("y"))
+        )
+      )
+    ),
+    Print("pi = "),
+    Out(Id("pi"))
+  ))
   def main(args: Array[String]): Unit = {
-    println("Hello, mur!")
+    val result = new Interpreter().run(prog)
+
+    println(result.output.mkString)
   }
 }
