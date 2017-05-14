ea429b901ebfb5358d71d8b0692bbdb97f4b7ebf
Maxim Gekk <max.gekk@gmail.com>
2017-05-14 21:12:59 +0200

    Added some comments to Expr

--- src/main/scala/mur/Expr.scala
+++ src/main/scala/mur/Expr.scala
@@ -1,24 +1,33 @@
 package mur
 
+// Intermediate representation for expressions
 sealed trait Expr
-case class Literal(value: AnyVal) extends Expr
-case class Brackets(expr: Expr) extends Expr
-case class Id(name: String) extends Expr
-case class Sequence(begin: Expr, end: Expr) extends Expr
+case class Literal(value: AnyVal) extends Expr // 10, 3.14
+case class Brackets(expr: Expr) extends Expr   // ()
+case class Id(name: String) extends Expr // identifer - a string like abc123
+case class Sequence(begin: Expr, end: Expr) extends Expr // {1, 100}
 
+// Arithmetic operator like +,-,*,/,^. Example: 1 + 2
 sealed trait Op extends Expr {
-  def left: Expr
-  def right: Expr
+  def left: Expr   // left operand - 1 in the example
+  def right: Expr  // right operand
 }
-case class Plus(left: Expr, right: Expr) extends Op
-case class Minus(left: Expr, right: Expr) extends Op
-case class Mul(left: Expr, right: Expr) extends Op
-case class Div(left: Expr, right: Expr) extends Op
-case class Pow(left: Expr, right: Expr) extends Op
+case class Plus(left: Expr, right: Expr) extends Op  // 1 + 2
+case class Minus(left: Expr, right: Expr) extends Op // 2.0 - 1
+case class Mul(left: Expr, right: Expr) extends Op   // 3 * 4
+case class Div(left: Expr, right: Expr) extends Op   // 6/3.3
+case class Pow(left: Expr, right: Expr) extends Op   // 2 ^ 8
 
+// Applying a lambda function to each element of a sequence:
+// map(sequence, x -> x + 1)
 case class MapSeq(seq: Expr, x: Id, expr: Expr) extends Expr
+// Reducing a suquence by applying of a lambda function:
+// reduce(sequence, init, x y -> x + y)
 case class ReduceSeq(seq: Expr, init: Expr, x: Id, y: Id, expr: Expr) extends Expr
 
+// A value of a calculated expression. Support 2 kind of values:
+// - single value - Num (integer) or Real (double)
+// - sequence of single values - NumSeq and RealSeq
 sealed trait ExprValue
 case class Num(value: Int) extends ExprValue {
   override def toString: String = value.toString
@@ -28,7 +37,7 @@ case class Real(value: Double) extends ExprValue {
   override def toString: String = value.toString
 }
 case class RealSeq(seq: Seq[Double]) extends ExprValue
-
+// Result of calculation of an expression: value or error
 case class ExprResult(value: Option[ExprValue], error: Option[String] = None)
 
 object Expr {
@@ -39,15 +48,16 @@ object Expr {
       case Literal(v) => ExprResult(None, Some(s"Invalid literal type (${v.getClass.getName})"))
       case Brackets(expr) => calc(expr, ctx)
       case Id(name) =>
-        ctx.ids.get(name) match {
+        ctx.ids.get(name) match { // Look up the identifier in the ids map in context
           case None => ExprResult(None, Some(s"Identifier $name is not defined"))
           case value => ExprResult(value)
         }
       case Sequence(begin, end) =>
+        // Materialise begin and end of the expression. Supported only Nums
         val (beginResult, endResult) = (calc(begin, ctx), calc(end, ctx))
         (beginResult, endResult) match {
           case (ExprResult(Some(Num(bv)), None), ExprResult(Some(Num(ev)), None)) =>
-            if (bv <= ev)
+            if (bv <= ev) // Supported only ascending sequence of numbers
               ExprResult(Some(NumSeq(bv to ev)), None)
             else
               ExprResult(None, Some(s"Wrong params of the sequence: ${bv}..${ev}"))
@@ -55,6 +65,8 @@ object Expr {
           case (_, error @ ExprResult(None, _)) => error
         }
       case op: Op =>
+        // Materialisation of left operand and after that right operand even if
+        // the left operand is invalid.
         val (leftValue, rightValue) = (calc(op.left, ctx), calc(op.right, ctx))
         (leftValue, rightValue) match {
           case (lErr @ ExprResult(None, _), _) => lErr
@@ -72,7 +84,8 @@ object Expr {
       case reduce: ReduceSeq => MapReduce.calc(reduce, ctx)
     }
   }
-
+  // Create a new copy of the expression by replacing the given identifier
+  // by provided literal.
   def transform(orig: Expr, from: Id, to: Literal): Expr = {
     orig match {
       case _: Literal => orig
