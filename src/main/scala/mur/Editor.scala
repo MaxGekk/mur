e2816e8db7e65c1c46dcb05cace855a150dc3576
Maxim Gekk <max.gekk@gmail.com>
2017-05-15 19:50:28 +0200

    Refactoring: timer handler

--- src/main/scala/mur/Editor.scala
+++ src/main/scala/mur/Editor.scala
@@ -6,29 +6,30 @@ import scala.swing._
 import scala.swing.event._
 
 object Editor extends SimpleSwingApplication {
-  val text = new TextArea(Main.text)
+  val input = new TextArea(Main.text)
   val output = new TextArea("No errors\n")
   val interpreter = new Interpreter()
   def top = new MainFrame{
     title = "MuR Editor"
     preferredSize = new Dimension(500, 500)
     contents = new BoxPanel(Orientation.Vertical) {
-      contents += new ScrollPane(text)
+      contents += new ScrollPane(input)
       contents += new ScrollPane(output)
     }
-    listenTo(text.keys)
+    listenTo(input.keys)
 
     val timerListener = new ActionListener {
       override def actionPerformed(actionEvent: ActionEvent) = {
-        Parsers.parse(text.text) match {
-          case Left(err) => output.append("Parsing error: " + err + "\n")
+        val outstr = Parsers.parse(input.text) match {
+          case Left(err) => "Parsing error: " + err
           case Right(prog) =>
             val result = interpreter.run(prog)
             result match {
-              case Result(out, None) => output.append(out.mkString + "\n")
-              case Result(_, Some(err)) => output.append("Error: " + err + "\n")
+              case Result(out, None) => out.mkString
+              case Result(_, Some(err)) => "Error: " + err
             }
         }
+        output.append(outstr + "\n")
       }
     }
     val timer = new javax.swing.Timer(1000, timerListener)
