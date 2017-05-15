a85dd08c2a07a7667eb4899ef268073700878dce
Maxim Gekk <max.gekk@gmail.com>
2017-05-15 20:15:40 +0200

    Non-blocking editor on akka actors

--- build.sbt
+++ build.sbt
@@ -7,11 +7,13 @@ scalaVersion := "2.12.2"
 val scalaTestV = "3.0.1"
 val parserV = "1.0.6"
 val swingV = "2.0.0"
+val akkaV = "2.5.1"
 
 coverageEnabled := false
 
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % scalaTestV % "test",
   "org.scala-lang.modules" %% "scala-parser-combinators" % parserV,
-  "org.scala-lang.modules" %% "scala-swing" % swingV
+  "org.scala-lang.modules" %% "scala-swing" % swingV,
+  "com.typesafe.akka" %% "akka-actor" % akkaV
 )
--- src/main/scala/mur/Editor.scala
+++ src/main/scala/mur/Editor.scala
@@ -2,13 +2,49 @@ package mur
 
 import java.awt.event.{ActionEvent, ActionListener}
 
+import akka.actor.{Actor, ActorSystem, OneForOneStrategy, Props}
+import akka.actor.SupervisorStrategy.Restart
+import akka.routing.{DefaultResizer, SmallestMailboxPool}
+
+import scala.concurrent.duration.Duration
 import scala.swing._
 import scala.swing.event._
 
+case class NewInput(text: String)
+
+class Worker extends Actor {
+  val interpreter = new Interpreter()
+  var counter = 0L
+
+  def receive = {
+    case NewInput(text) =>
+      val outstr = Parsers.parse(text) match {
+        case Left(err) => "Parsing error: " + err
+        case Right(prog) =>
+          val result = interpreter.run(prog)
+          result match {
+            case Result(out, None) => out.mkString
+            case Result(_, Some(err)) => "Error: " + err
+          }
+      }
+      counter += 1
+      Swing.onEDT{ Editor.output.append(s"[$counter] $outstr\n") }
+  }
+}
+
 object Editor extends SimpleSwingApplication {
   val input = new TextArea(Main.text)
-  val output = new TextArea("No errors\n")
-  val interpreter = new Interpreter()
+  val output = new TextArea("[0] No errors\n")
+  val system = ActorSystem("mur-editor")
+  val WORKERS_AMOUNT = 2
+  val workers = system.actorOf(
+    props = SmallestMailboxPool(WORKERS_AMOUNT).
+      withSupervisorStrategy(OneForOneStrategy(-1, Duration.Inf) { case _ => Restart }).
+      withResizer(DefaultResizer(lowerBound = WORKERS_AMOUNT, upperBound = 2 * WORKERS_AMOUNT)).
+      props(Props(classOf[Worker])),
+    name = "worker"
+  )
+
   def top = new MainFrame{
     title = "MuR Editor"
     preferredSize = new Dimension(500, 500)
@@ -20,16 +56,7 @@ object Editor extends SimpleSwingApplication {
 
     val timerListener = new ActionListener {
       override def actionPerformed(actionEvent: ActionEvent) = {
-        val outstr = Parsers.parse(input.text) match {
-          case Left(err) => "Parsing error: " + err
-          case Right(prog) =>
-            val result = interpreter.run(prog)
-            result match {
-              case Result(out, None) => out.mkString
-              case Result(_, Some(err)) => "Error: " + err
-            }
-        }
-        output.append(outstr + "\n")
+        workers ! NewInput(input.text)
       }
     }
     val timer = new javax.swing.Timer(1000, timerListener)
