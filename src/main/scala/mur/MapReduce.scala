36b23b0f2c0941c117515df1dec9a82e67ff69d7
Maxim Gekk <max.gekk@gmail.com>
2017-05-18 14:16:50 +0200

    Optimization and bug fix parallel reduce/map

--- src/main/scala/mur/MapReduce.scala
+++ src/main/scala/mur/MapReduce.scala
@@ -6,46 +6,48 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 /** Calculating results of the map and reduce operations */
 object MapReduce {
-  val CHUNK_SIZE = 8096
+  val CHUNK_SIZE = 8192
 
-  def calc(map: MapSeq, ctx: Context): ExprResult = {
-    def mapSeq(seq: List[AnyVal]): ExprResult = {
-      val sliced = slice(seq, ctx)
+  def calc(op: MapSeq, ctx: Context): ExprResult = {
+    def map(vals: List[AnyVal]): ExprResult = {
+      val sliced = slice(vals, ctx)
       val futures = sliced.map { case (chunk, context) =>
         Future {
           val res = chunk.map { elem =>
-            context.ids.put(map.x.name, toNumValue(elem))
-            Expr.calc(map.expr, context)
+            context.ids.put(op.x.name, toNumValue(elem))
+            Expr.calc(op.expr, context)
           }
-          merge(res)
+          union(res)
         }
       }
-      val res = Await.result(Future.sequence(futures), Duration.Inf)
-      merge(res.toIterable)
+      val res = Await.result(Future.sequence(futures.toSeq.reverse), Duration.Inf)
+      union(res)
     }
     // Materialisation of the first parameter - a sequence
-    val range = Expr.calc(map.seq, ctx)
+    val range = Expr.calc(op.seq, ctx)
     range match {
       // Extract sequence of integers or doubles
-      case ExprResult(Some(NumSeq(seq)), _) => mapSeq(seq)
-      case ExprResult(Some(RealSeq(seq)), _) => mapSeq(seq)
+      case ExprResult(Some(NumSeq(seq)), _) => map(seq)
+      case ExprResult(Some(RealSeq(seq)), _) => map(seq)
       case ExprResult(Some(unknown), _) =>
         ExprResult(None, Some(s"map works over sequences only"))
       case error => error
     }
   }
-  // Reduce a sequence: reduce(seq, init, x y -> x + y)
-  def calc(reduce: ReduceSeq, ctx: Context): ExprResult = {
-    def reduceSeq(seq: List[AnyVal]): ExprResult = {
-      def reduceOp(x: AnyVal, y: AnyVal, context: Context): ExprResult = {
-        context.ids.put(reduce.x.name, toNumValue(x))
-        context.ids.put(reduce.y.name, toNumValue(y))
 
-        Expr.calc(reduce.expr, context)
+  // Reduce a sequence: reduce(seq, init, x y -> x + y)
+  def calc(op: ReduceSeq, ctx: Context): ExprResult = {
+    val init = Expr.calc(op.init, ctx)
+
+    def reduce(vals: List[AnyVal]): ExprResult = {
+      def applyLambda(x: AnyVal, y: AnyVal, context: Context): ExprResult = {
+        context.ids.put(op.x.name, toNumValue(x))
+        context.ids.put(op.y.name, toNumValue(y))
+
+        Expr.calc(op.expr, context)
       }
       // Materialise init (or neutral value) parameter
-      val init = Expr.calc(reduce.init, ctx)
-      val sliced = slice(seq, ctx)
+      val sliced = slice(vals, ctx)
       val futures = sliced.map { case (chunk, context) =>
         Future {
           // Iterate over the sequence staring from the init param and calculate
@@ -54,28 +56,37 @@ object MapReduce {
             // Keep error and return it
             case (error @ ExprResult(None, _), _) => error
 
-            case (ExprResult(Some(Num(n)), _), elem) => reduceOp(n, elem, context)
-            case (ExprResult(Some(Real(n)), _), elem) => reduceOp(n, elem, context)
+            case (ExprResult(Some(Num(n)), _), elem) => applyLambda(n, elem, context)
+            case (ExprResult(Some(Real(n)), _), elem) => applyLambda(n, elem, context)
             case (ExprResult(Some(some), _), elem) =>
               ExprResult(None, Some(s"reduce produces wrong type: ${some.getClass.getName}"))
           }
         }
       }
       val res = Await.result(Future.sequence(futures), Duration.Inf)
-      merge(res.toIterable)
+      reduceRes(union(res.toIterable))
     }
-    // Materialise the sequence - first parameter
-    val range = Expr.calc(reduce.seq, ctx)
-    range match {
-      case ExprResult(Some(NumSeq(seq)), _) => reduceSeq(seq)
-      case ExprResult(Some(RealSeq(seq)), _) => reduceSeq(seq)
+    def reduceRes(results: ExprResult): ExprResult = results match {
+      case ExprResult(Some(NumSeq(seq)), _) if seq.isEmpty => init
+      case ExprResult(Some(NumSeq(seq)), _) if seq.length == 1 =>
+        ExprResult(Some(Num(seq.head)))
+      case ExprResult(Some(NumSeq(seq)), _) => reduce(seq)
+
+      case ExprResult(Some(RealSeq(seq)), _) if seq.isEmpty => init
+      case ExprResult(Some(RealSeq(seq)), _) if seq.length == 1 =>
+        ExprResult(Some(Real(seq.head)))
+      case ExprResult(Some(RealSeq(seq)), _) => reduce(seq)
       case ExprResult(Some(_), _) =>
         ExprResult(None, Some(s"reduce works over sequences only"))
       case error => error
     }
+
+    // Materialise the sequence - first parameter
+    val range = Expr.calc(op.seq, ctx)
+    reduceRes(range)
   }
 
-  def merge(results: Iterable[ExprResult]): ExprResult = {
+  def union(results: Iterable[ExprResult]): ExprResult = {
     results.foldRight(ExprResult(Some(NumSeq(List())))) {
       // Keep the first error and return it (ignore other values)
       case (error @ ExprResult(None, _), _) => error
