d47bc91c8e91671cb04a4a76adf5a7bdbe324f41
Maxim Gekk <max.gekk@gmail.com>
2017-05-23 15:30:06 +0200

    Moving Worker to a separate file

--- src/main/scala/mur/Editor.scala
+++ src/main/scala/mur/Editor.scala
@@ -1,67 +1,17 @@
 package mur
 
 import java.awt.event.{ActionEvent, ActionListener}
-
-import akka.actor.{Actor, ActorSystem, OneForOneStrategy, Props}
-import akka.actor.SupervisorStrategy.Restart
-import akka.routing.{DefaultResizer, SmallestMailboxPool}
+import akka.actor.ActorSystem
 import com.typesafe.config.ConfigFactory
-
-import scala.concurrent.duration.Duration
 import scala.swing._
 import scala.swing.event._
-
-case class NewInput(text: String)
-
-class Worker extends Actor {
-  val interpreter = new Interpreter()
-  var counter = 0L
-
-  def receive = {
-    case NewInput(text) =>
-      val (outstr, error) = try {
-        Parsers.parse(text) match {
-          case Left(err) => ("Parsing error: " + err, Some(err))
-          case Right(prog) =>
-            val result = interpreter.run(prog)
-            result match {
-              case Result(out, None) => (out.mkString, None)
-              case Result(_, Some(err)) => ("Error: " + err, Some(err))
-            }
-        }
-      } catch {
-        case e: Throwable => ("Exception:" + e.toString, None)
-      }
-      counter += 1
-      output(outstr, error)
-  }
-
-  def output(outstr: String, error: Option[mur.Error]): Unit = {
-    Swing.onEDT {
-      Editor.output.peer.setText(s"[$counter] $outstr\n")
-
-      val highlighter = Editor.input.peer.getHighlighter()
-      highlighter.removeAllHighlights()
-      error foreach { e =>
-        import javax.swing.text.DefaultHighlighter
-        import java.awt.Color
-        val painter = new DefaultHighlighter.DefaultHighlightPainter(Color.ORANGE)
-        highlighter.addHighlight(
-          Editor.input.peer.getLineStartOffset(e.line - 1) + e.column - 1,
-          Editor.input.peer.getLineEndOffset(e.line - 1),
-          painter
-        )
-      }
-    }
-  }
-}
 
 object Editor extends SimpleSwingApplication {
   val config = ConfigFactory.load
   val input = new TextArea(Main.text)
   val output = new TextArea("[0] No errors\n")
   val actorSystem = ActorSystem("mur-editor")
-  val workers = actorSystem.actorOf(props = workerSettings, name = "worker")
+  val workers = actorSystem.actorOf(props = Worker.settings, name = "worker")
 
   def top = new MainFrame {
     title = "MuR Editor"
@@ -90,18 +40,6 @@ object Editor extends SimpleSwingApplication {
   override def shutdown(): Unit = {
     actorSystem.stop(workers)
     actorSystem.terminate()
-  }
-
-  def workerSettings = {
-    val WORKERS_AMOUNT = config.getInt("akka.workers-amount")
-
-    SmallestMailboxPool(WORKERS_AMOUNT).
-      withSupervisorStrategy(OneForOneStrategy(-1, Duration.Inf) { case _ => Restart }).
-      withResizer(DefaultResizer(
-        lowerBound = WORKERS_AMOUNT,
-        upperBound = WORKERS_AMOUNT
-      )).
-      props(Props(classOf[Worker]))
   }
 }
 
---
+++ src/main/scala/mur/Worker.scala
@@ -0,0 +1,70 @@
+package mur
+
+import akka.actor.{Actor, OneForOneStrategy, Props}
+import akka.actor.SupervisorStrategy.Restart
+import akka.routing.{DefaultResizer, SmallestMailboxPool}
+import mur.Editor.config
+
+import scala.concurrent.duration.Duration
+import scala.swing.Swing
+
+// A message with new entered text
+case class NewInput(text: String)
+
+class Worker extends Actor {
+  val interpreter = new Interpreter()
+  var counter = 0L
+
+  def receive = {
+    case NewInput(text) =>
+      val (outstr, error) = try {
+        Parsers.parse(text) match {
+          case Left(err) => ("Parsing error: " + err, Some(err))
+          case Right(prog) =>
+            val result = interpreter.run(prog)
+            result match {
+              case Result(out, None) => (out.mkString, None)
+              case Result(_, Some(err)) => ("Error: " + err, Some(err))
+            }
+        }
+      } catch {
+        case e: Throwable => ("Exception:" + e.toString, None)
+      }
+      counter += 1
+      Worker.output(counter, outstr, error)
+  }
+}
+
+object Worker {
+  def settings = {
+    val WORKERS_AMOUNT = config.getInt("akka.workers-amount")
+
+    SmallestMailboxPool(WORKERS_AMOUNT).
+      withSupervisorStrategy(OneForOneStrategy(-1, Duration.Inf) { case _ => Restart }).
+      withResizer(DefaultResizer(
+        lowerBound = WORKERS_AMOUNT,
+        upperBound = WORKERS_AMOUNT
+      )).
+      props(Props(classOf[Worker]))
+  }
+
+  def output(counter: Long, outstr: String, error: Option[mur.Error]): Unit = {
+    Swing.onEDT {
+      Editor.output.peer.setText(s"[$counter] $outstr\n")
+
+      val highlighter = Editor.input.peer.getHighlighter()
+      highlighter.removeAllHighlights()
+      error foreach { e =>
+        import javax.swing.text.DefaultHighlighter
+        import java.awt.Color
+        val painter = new DefaultHighlighter.DefaultHighlightPainter(Color.ORANGE)
+        highlighter.addHighlight(
+          Editor.input.peer.getLineStartOffset(e.line - 1) + e.column - 1,
+          Editor.input.peer.getLineEndOffset(e.line - 1),
+          painter
+        )
+      }
+    }
+  }
+}
+
