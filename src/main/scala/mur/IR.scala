1423f81d32921fbdcda96cb68bf9cc31c8123ace
Maxim Gekk <max.gekk@gmail.com>
2017-05-23 18:29:24 +0200

    Fix typo: identifier

--- src/main/scala/mur/IR.scala
+++ src/main/scala/mur/IR.scala
@@ -5,6 +5,6 @@ import scala.util.parsing.input.Positional
 sealed trait Stmt extends Positional
 case class Print(str: String) extends Stmt
 case class Out(expr: Expr) extends Stmt
-case class VarDef(identifer: String, expr: Expr) extends Stmt
+case class VarDef(identifier: String, expr: Expr) extends Stmt
 
 case class Program(stmts: Seq[Stmt])
