0c886814d83641c5ddeeee32463b0bf3de96c877
Maxim Gekk <max.gekk@gmail.com>
2017-05-23 10:45:48 +0200

    Highlight position of errors

--- src/main/scala/mur/Editor.scala
+++ src/main/scala/mur/Editor.scala
@@ -18,7 +18,7 @@ class Worker extends Actor {
 
   def receive = {
     case NewInput(text) =>
-      val (outstr, error: Option[mur.Error]) = try {
+      val (outstr, error) = try {
         Parsers.parse(text) match {
           case Left(err) => ("Parsing error: " + err, Some(err))
           case Right(prog) =>
@@ -29,25 +29,29 @@ class Worker extends Actor {
             }
         }
       } catch {
-        case e: Throwable => "Exception:" + e.toString
+        case e: Throwable => ("Exception:" + e.toString, None)
       }
       counter += 1
-      Swing.onEDT{
-        Editor.output.peer.setText(s"[$counter] $outstr\n")
+      output(outstr, error)
+  }
 
-        val highlighter = Editor.input.peer.getHighlighter()
-        highlighter.removeAllHighlights()
-        error foreach { e =>
-          import javax.swing.text.DefaultHighlighter
-          import java.awt.Color
-          val painter = new DefaultHighlighter.DefaultHighlightPainter(Color.ORANGE)
-          highlighter.addHighlight(
-            Editor.input.peer.getLineStartOffset(e.line - 1),
-            Editor.input.peer.getLineEndOffset(e.line - 1),
-            painter
-          )
-        }
+  def output(outstr: String, error: Option[mur.Error]): Unit = {
+    Swing.onEDT {
+      Editor.output.peer.setText(s"[$counter] $outstr\n")
+
+      val highlighter = Editor.input.peer.getHighlighter()
+      highlighter.removeAllHighlights()
+      error foreach { e =>
+        import javax.swing.text.DefaultHighlighter
+        import java.awt.Color
+        val painter = new DefaultHighlighter.DefaultHighlightPainter(Color.ORANGE)
+        highlighter.addHighlight(
+          Editor.input.peer.getLineStartOffset(e.line - 1) + e.column - 1,
+          Editor.input.peer.getLineEndOffset(e.line - 1),
+          painter
+        )
       }
+    }
   }
 }
 
@@ -90,3 +94,4 @@ object Editor extends SimpleSwingApplication {
     actorSystem.terminate()
   }
 }
+
